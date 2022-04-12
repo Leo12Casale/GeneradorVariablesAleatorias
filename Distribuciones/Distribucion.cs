9dafe76ce52b406ed3b960befe6500fb7bcf075f
LeonelCasale <leonelcasale@gmail.com>
2022-04-12 20:32:40 -0300

    Distribucion Uniforme lista

--- Distribuciones/Distribucion.cs
+++ Distribuciones/Distribucion.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace TP3_VariablesAleatorias.Distribuciones
 {
-    public interface Distribucion
+    public abstract class Distribucion
     {
+        protected double[] generarRNDs(int cantidadRNDsNecesarios)
+        {
+            double[] RNDs = new double[cantidadRNDsNecesarios];
+
+            Random randNum = new Random();
+            for (int i = 0; i < RNDs.Length; i++)
+            {
+                RNDs[i] = randNum.NextDouble();
+            }
+            return RNDs;
+        }
     }
 }
--- Distribuciones/Uniforme.cs
+++ Distribuciones/Uniforme.cs
@@ -8,5 +8,35 @@ namespace TP3_VariablesAleatorias.Distribuciones
 {
     public class Uniforme : Distribucion
     {
+        //Atributos
+        private double a;
+        private double b;
+        private double[] serieGenerada;
+
+        //Constructor
+        public Uniforme(double a, double b)
+        {
+            this.A = a;
+            this.B = b;
+        }
+
+        //Getters y Setters
+        public double A { get => a; set => a = value; }
+        public double B { get => b; set => b = value; }
+        public double[] SerieGenerada { get => serieGenerada; set => serieGenerada = value; }
+
+        //Métodos de la clase
+        public double[] generarSerie(int cantidadNumerosAGenerar)
+        {
+            this.serieGenerada = new double[cantidadNumerosAGenerar];
+            //Creamos los números RNDs
+            double[] numerosRND = generarRNDs(cantidadNumerosAGenerar);
+            //Generamos los números aleatorios uniformes
+            for (int i = 0; i < cantidadNumerosAGenerar; i++)
+            {
+                this.serieGenerada[i] = this.A + numerosRND[i] * (this.B - this.A);
+            }
+            return serieGenerada;
+        }
     }
 }
