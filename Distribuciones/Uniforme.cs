9e3c7ed69055ee071dedfcf656e2872ba0a248f8
Federico Host <federico.host@gmail.com>
2022-04-15 11:30:58 -0300

    Clase Uniforme

--- Distribuciones/Uniforme.cs
+++ Distribuciones/Uniforme.cs
@@ -11,7 +11,7 @@ namespace TP3_VariablesAleatorias.Distribuciones
         //Atributos
         private double a;
         private double b;
-        private double[] serieGenerada;
+      
 
         //Constructor
         public Uniforme(double a, double b)
@@ -23,11 +23,12 @@ namespace TP3_VariablesAleatorias.Distribuciones
         //Getters y Setters
         public double A { get => a; set => a = value; }
         public double B { get => b; set => b = value; }
-        public double[] SerieGenerada { get => serieGenerada; set => serieGenerada = value; }
 
         //Métodos de la clase
         public override double[] generarSerie(int cantidadNumerosAGenerar)
         {
+            TamañoMuestra = cantidadNumerosAGenerar;
+            CantidadIntervalos = (int)(Math.Sqrt(TamañoMuestra));
             this.serieGenerada = new double[cantidadNumerosAGenerar];
             //Creamos los números RNDs
             double[] numerosRND = generarRNDs(cantidadNumerosAGenerar);
@@ -37,6 +38,70 @@ namespace TP3_VariablesAleatorias.Distribuciones
                 this.serieGenerada[i] = this.A + numerosRND[i] * (this.B - this.A);
             }
             return serieGenerada;
+        }
+
+        public override int getCantDatosEmpiricos()
+        {
+            return 0;
+        }
+
+
+        public override double[] getFrecuenciasEsperadas()
+        {
+            frecuenciasEsperadas = new double[cantidadIntervalos];
+            for (int i = 0; i < serieGenerada.Length; i++)
+            {
+                frecuenciasEsperadas[i] = TamañoMuestra / cantidadIntervalos; 
+            }
+            return frecuenciasEsperadas;               
+        }
+
+        public override int[] getFrecuenciasObservadas()
+        {
+            frecuenciasObservadas = new int[cantidadIntervalos];
+            double tamañoIntervalo = 1 / (double)cantidadIntervalos;
+            for (int i = 0; i < serieGenerada.Length; i++)
+            {
+
+                frecuenciasObservadas[(int)Math.Floor(serieGenerada[i] / tamañoIntervalo)] += 1;
+            }
+            return frecuenciasObservadas;
+        }
+
+        public override double[] getIntervalosDesde()
+        {
+            double[] intervalosDesde = new double[cantidadIntervalos];
+            double acumulador = 0;
+            double tamañoIntervalo = 1 / (double)cantidadIntervalos;
+            for (int i = 0; i < intervalosDesde.Length; i++)
+            {
+                intervalosDesde[i] = acumulador;
+                acumulador += tamañoIntervalo;
+            }
+            return intervalosDesde;
+        }
+
+        public override double[] getIntervalosHasta()
+        {
+            double[] intervalosHasta = new double[cantidadIntervalos];
+            double acumulador = tamañoMuestra;
+            double tamañoIntervalo = 1 / (double)cantidadIntervalos;
+            for (int i = 0; i < intervalosHasta.Length; i++)
+            {
+                intervalosHasta[i] = acumulador;
+                acumulador += tamañoIntervalo;
+            }
+            return intervalosHasta;
+        }
+
+        public override double[] getProbabilidadEsperada()
+        {
+            double [] probabilidadesEsperadas = new double[cantidadIntervalos];
+            for (int i = 0; i < probabilidadesEsperadas.Length; i++)
+            {
+                probabilidadesEsperadas[i] = frecuenciasEsperadas[i] / tamañoMuestra;
+            }
+            return probabilidadesEsperadas;
         }
     }
 }
