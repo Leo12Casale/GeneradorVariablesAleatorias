05a35718f21327506c0050c95abdc7286288b26a
ValentinTT <valentintapiatorti@gmail.com>
2022-04-18 17:19:26 -0300

    ambiente de prueba

--- Distribuciones/Uniforme.cs
+++ Distribuciones/Uniforme.cs
@@ -28,15 +28,17 @@ namespace TP3_VariablesAleatorias.Distribuciones
         public override double[] generarSerie(int cantidadNumerosAGenerar)
         {
             TamañoMuestra = cantidadNumerosAGenerar;
-            CantidadIntervalos = (int)(Math.Sqrt(TamañoMuestra));
-            this.serieGenerada = new double[cantidadNumerosAGenerar];
+            CantidadIntervalos = 5;// (int)(Math.Sqrt(TamañoMuestra));
+            this.serieGenerada = new double[]{ 0.15, 0.22, 0.41, 0.65, 0.84, 0.81, 0.62, 0.45, 0.32, 0.07, 0.11, 0.29, 0.58, 0.73, 0.93, 0.97, 0.79, 0.55, 0.35, 0.09, 0.99, 0.51, 0.35, 0.02, 0.19, 0.24, 0.98, 0.10, 0.31, 0.17};
+            //Calcule arrays po pe, fe, fo
+            /*new double[cantidadNumerosAGenerar];
             //Creamos los números RNDs
             double[] numerosRND = generarRNDs(cantidadNumerosAGenerar);
             //Generamos los números aleatorios uniformes
             for (int i = 0; i < cantidadNumerosAGenerar; i++)
             {
                 this.serieGenerada[i] = this.A + numerosRND[i] * (this.B - this.A);
-            }
+            }*/
             return serieGenerada;
         }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TP3_VariablesAleatorias.Distribuciones;
+using TP3_VariablesAleatorias.PruebasBondad;
 
 namespace TP3_VariablesAleatorias
 {
@@ -15,6 +17,21 @@ namespace TP3_VariablesAleatorias
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Uniforme uni = new Uniforme(0, 1);
+            uni.generarSerie(30);
+            uni.getFrecuenciasObservadas();
+            uni.getProbabilidadObservada();
+            uni.getProbabilidadEsperada();
+            uni.getFrecuenciasEsperadas();
+            ChiCuadrado chi = new ChiCuadrado(uni);
+
+            var (resB, chiC, chiT, cadena) = chi.realizarPrueba();
+
+            Console.WriteLine(resB.ToString(), chiC, chiT, cadena);
         }
     }
 }
